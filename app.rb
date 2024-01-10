2b8dac7cb78dabcc32429a778481e636c59348ab
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-01-10 03:35:15 +0000

    Added access to API

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_key = ENV['NASA_API']
+  api_url = "https://api.nasa.gov/planetary/apod?api_key=#{api_key}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  # @symbols = parsed_data.fetch("currencies")
+ erb(:homepage)
 end
