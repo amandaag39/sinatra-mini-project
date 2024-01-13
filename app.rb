3d5ac4555c6b9a875e5b0db7d22b098132172155
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-01-13 02:47:39 +0000

    Added quote generator and some css styling

--- app.rb
+++ app.rb
@@ -20,5 +20,19 @@ get("/") do
   @date_data = parsed_data["date"]
   @title_data = parsed_data["title"]
 
+  quote_api_url = "https://stoic-quotes.com/api/quote"
+
+  # use HTTP.get to retrieve the API information
+  quote_raw_data = HTTP.get(quote_api_url)
+
+  # convert the raw request to a string
+  quote_raw_data_string = quote_raw_data.to_s
+
+  # convert the string to JSON
+  quote_parsed_data = JSON.parse(quote_raw_data_string)
+
+  #get the quote
+  @quote_text = quote_parsed_data["text"]
+  @quote_author = quote_parsed_data["author"]
  erb(:homepage)
 end
